708e4c046eded2fee563182fb41fe23bb6656690
Ranjithkumar Matheswaran <me@iranjith4.com>
2016-04-05 10:48:45 +0530

    Initial Commit

---
+++ perkwalkthrough/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  perkwalkthrough
+//
+//  Created by Ranjithkumar on 05/04/16.
+//  Copyright © 2016 womistudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ perkwalkthrough/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  perkwalkthrough
+//
+//  Created by Ranjithkumar on 05/04/16.
+//  Copyright © 2016 womistudios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
